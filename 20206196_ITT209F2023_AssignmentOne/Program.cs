8a229a7d6602ca5153ac776abfe9d7c560893347
charmawhorms <charmawhorms27@gmail.com>
2023-09-28 13:03:28 -0500

    prompt the user to enter 10 numbers and using a for loop and save it to the array

--- 20206196_ITT209F2023_AssignmentOne/Program.cs
+++ 20206196_ITT209F2023_AssignmentOne/Program.cs
@@ -8,8 +8,28 @@ namespace _20206196_ITT209F2023_AssignmentOne
 {
     internal class Program
     {
+        /*
+         * ===============================
+         * ID: 20206196
+         * Student Name: Charma Whorms
+         * ===============================
+         */
+
         static void Main(string[] args)
         {
+            //Declaring an array that can holds 10 numbers
+            int[] userNumbers = new int[10];
+
+            Console.WriteLine("Welcome to the Array Sorter!");
+            Console.WriteLine("Enter 10 numbers");
+
+            for (int i = 0; i < userNumbers.Length; i++)
+            {
+                Console.WriteLine("Enter a number: ");
+                userNumbers[i] = Convert.ToInt32(Console.ReadLine());                
+            }
+
+            Console.ReadKey();
         }
     }
 }
